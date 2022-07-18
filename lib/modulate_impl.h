26547c99c8e4217d3ad3e2053ccfd2df545b3a9b
Atsushi0803 <atsushi.7624@gmail.com>
2022-07-18 11:49:45 +0900

    codewordの作成まで終了

--- lib/modulate_impl.h
+++ lib/modulate_impl.h
@@ -1,62 +1,76 @@
 #ifndef INCLUDED_LORA_MODULATE_IMPL_H
 #define INCLUDED_LORA_MODULATE_IMPL_H
 
-#include <lora_sdr/modulate.h>
 #include <gnuradio/io_signature.h>
-#include <iostream>
-#include <fstream>
-
+#include <lora_sdr/modulate.h>
 #include <lora_sdr/utilities.h>
 
-namespace gr {
-  namespace lora_sdr {
+#include <fstream>
+#include <iostream>
 
-    class modulate_impl : public modulate
-    {
-     private:
-        uint8_t m_sf; ///< Transmission spreading factor
-        uint32_t m_samp_rate; ///< Transmission sampling rate
-        uint32_t m_bw; ///< Transmission bandwidth (Works only for samp_rate=bw)
-        uint32_t m_number_of_bins; ///< number of bin per loar symbol
-        uint32_t m_samples_per_symbol; ///< samples per symbols(Works only for 2^sf)
-        std::vector<uint16_t> m_sync_words; ///< sync words (network id) 
-	
-	uint32_t send_data;
+namespace gr
+{
+namespace lora_sdr
+{
 
-        int m_os_factor; ///< ovesampling factor based on sampling rate and bandwidth
+class modulate_impl : public modulate
+{
+   private:
+    uint8_t m_sf;          ///< Transmission spreading factor
+    uint32_t m_samp_rate;  ///< Transmission sampling rate
+    uint32_t m_bw;  ///< Transmission bandwidth (Works only for samp_rate=bw)
+    uint32_t m_number_of_bins;  ///< number of bin per loar symbol
+    uint32_t
+        m_samples_per_symbol;  ///< samples per symbols(Works only for 2^sf)
+    std::vector<uint16_t> m_sync_words;  ///< sync words (network id)
 
-        int m_inter_frame_padding; ///< length in samples of zero append to each frame
+    uint32_t send_data;
 
-        int m_frame_len;///< leng of the frame in number of items
-       
+    int m_os_factor;  ///< ovesampling factor based on sampling rate and
+                      ///< bandwidth
 
-        std::vector<gr_complex> m_upchirp; ///< reference upchirp
-        std::vector<gr_complex> m_downchirp; ///< reference downchirp
+    int m_inter_frame_padding;  ///< length in samples of zero append to each
+                                ///< frame
 
-        
+    int m_frame_len;  ///< leng of the frame in number of items
 
-        uint n_up; ///< number of upchirps in the preamble
-        int32_t symb_cnt; ///< counter of the number of lora symbols sent
-        uint32_t preamb_symb_cnt; ///< counter of the number of preamble symbols output
-        uint32_t padd_cnt; ///< counter of the number of null symbols output after each frame
-        uint64_t frame_cnt; ///< counter of the number of frame sent
-        unsigned int m_send=0;
+    std::vector<gr_complex> m_upchirp;    ///< reference upchirp
+    std::vector<gr_complex> m_downchirp;  ///< reference downchirp
 
+    uint n_up;         ///< number of upchirps in the preamble
+    int32_t symb_cnt;  ///< counter of the number of lora symbols sent
+    uint32_t
+        preamb_symb_cnt;  ///< counter of the number of preamble symbols output
+    uint32_t padd_cnt;   ///< counter of the number of null symbols output after
+                         ///< each frame
+    uint64_t frame_cnt;  ///< counter of the number of frame sent
 
-     public:
-      modulate_impl(uint8_t sf, uint32_t samp_rate, uint32_t bw, std::vector<uint16_t> sync_words);
-      ~modulate_impl();
+    ///////////////// APCMA edited by ATsushi.N ////////////////////////////////
+    uint32_t m_send_index = 0;
+    uint32_t start_var;
+    uint32_t end_var;
+    std::vector<uint32_t> l_send_data;
+    uint32_t m_send_data;
+    uint32_t m_nbit;
+    uint32_t m_npulse;
+    uint32_t m_length_c;
+    std::vector<uint32_t> m_codeword;
+    std::vector<uint32_t> l_interval;
 
-      // Where all the action really happens
-      void forecast (int noutput_items, gr_vector_int &ninput_items_required);
+   public:
+    modulate_impl(uint8_t sf, uint32_t samp_rate, uint32_t bw,
+                  std::vector<uint16_t> sync_words);
+    ~modulate_impl();
 
-      int general_work(int noutput_items,
-           gr_vector_int &ninput_items,
-           gr_vector_const_void_star &input_items,
-           gr_vector_void_star &output_items);
-    };
+    // Where all the action really happens
+    void forecast(int noutput_items, gr_vector_int &ninput_items_required);
 
-  } // namespace lora
-} // namespace gr
+    int general_work(int noutput_items, gr_vector_int &ninput_items,
+                     gr_vector_const_void_star &input_items,
+                     gr_vector_void_star &output_items);
+};
+
+}  // namespace lora_sdr
+}  // namespace gr
 
 #endif /* INCLUDED_LORA_MODULATE_IMPL_H */
